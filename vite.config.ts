2f38e9dc206373b50eb8a4a617c9608c784daf5e
ahmedAbouEissa <ahmed.abouissa.aai@gmail.com>
2025-08-14 02:39:51 +0300

    Edit GitHub setup Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/",
+  base: "/ecommerce-react-ts-vite-tailwind/",
   plugins: [react(), tailwindcss()],
 });
